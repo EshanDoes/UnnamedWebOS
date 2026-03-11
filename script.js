7428c1b92bd8ea6b224808ea283c0149397225f3
EshanDoes <eshan.shiraz@gmail.com>
2026-03-11 11:05:32 -0400

    Added more files and animations
    Added the Deltarune and Splatoon trivia, added work in progress character files, added more notes, changed the styling to be more compatible, added a disclaimer that the site isn't mobile compatible when you open it on a mobile device, and made it so that the scrollbar only appears when the window content has reached overflow. There are also work in progress options that can't be seen on the site itself, and a shakeBody() function that will never run under normal circumstances.

--- script.js
+++ script.js
@@ -21,23 +21,159 @@ var files = {
             name: "WarriorCats",
             type: "text",
             content: `The Prophecies Begin originally didn't have an official arc name, with most fans referring to it as The Original Series, until Kate Cary revealed in 2014 that it would officially be called, "The Prophecies Begin" with the launch of the paperback reprints.<br>In an interview with Victoria Holmes, she mentioned that Tigerstar's original name in the drafts of Into the Wild was Hammerclaw, but was changed because cats do not know what hammers are.<br>Firestar has the most chapters in his point of view in the entire series, totaling at 176 chapters.<br>Cherith has said that Firestar's "love" relationship with Spottedleaf was never shown often when she was alive because he was only a new apprentice at the time. She also feels that the relationship would not have gotten much farther due to the fact medicine cats cannot have mates.<br>Cherith says that if she could have made Ravenpaw a warrior, she would name him Ravenwing. He also has been mistakenly called Ravepaw.<br>Greystripe's name has been mistakenly spelled as "Grewpaw".`
+        },{
+            name: "Deltarune",
+            type: "text",
+            content: `Deltarune was originally supposed to release the paid version with chapters 3, 4, and 5, but had to be scaled down to just chapters 3 and 4.<br>The original concept for Deltarune came from a fever dream Toby Fox had in 2011 about the ending of a non-existant video game. After that, he wanted to make this video game. This means it's been about 15 years since the origin of Deltarune.<br>Spamton's dialogue seems shockingly similar to a commercial with a guy named Mike who was advertising Big Shot Soda, airing in 1997.<br>In the Spamton NEO fight, there was a glitch allowing you to spam charged shots without having to charge them. In a later update, instead of the glitch being patched out, using it instead made Spamton angry and caused the fight to be harder.<br>Every single voice line in Deltarune is done by Toby Fox.<br>Multiple songs like The Chase have a melody very similar to another song that was made by Toby Fox called Nightmare Knight, uploaded in early 2016 for Cucumber Quest. Nightmare Knight also parallels the song Black Knife, which data shows that composition for Black Knife was done in 2018.<br>Roulxs Kaard was originally going to have his own voice sound, however it was cut out for being too distracting. However, Tenna has a similar voice sound that's arguably more distracting. We want the Roulxs Kaard voice. Give it to us.`
+        },{
+            name: "Splatoon",
+            type: "text",
+            content: "aHR0cHM6Ly9kaXNjb3JkLmNvbS9jaGFubmVscy8xMjc5NjMxMTA0MzI3OTQ2MzAxLzEyODAzNTE3NzQxNTgzNTY0OTA"
+        }]
+    },{
+        name: "characterFiles",
+        type: "folder",
+        content: [{
+            name: "shifty",
+            type: "folder",
+            content: [{
+                name: "shiftyNotes",
+                type: "text",
+                content: `The main character of his story. He hasn't appeared in the collapse yet, although he will any minute now. It will just take some time.<br><br>
+                - Cursed to be edited by like 20 people<br>
+                - Knows many of the others in the files<br>
+                - Still friends with Mystery Man<br><br>
+                He's already well along his destined path, but he might bring others to change course. It's best to keep an eye on him.`
+            }]
+        },{
+            name: "mysteryMan",
+            type: "folder",
+            content: [{
+                name: "mmNotes",
+                type: "text",
+                content: `Not Gaster.`
+            }]
+        },{
+            name: "jen",
+            type: "folder",
+            content: [{
+                name: "jenNotes",
+                type: "text",
+                content: `A "normal human" who's managed to end up in a not-so-normal situation. It seems like she's quickly made friends, but how long until she moves on?<br><br>
+                - Good at hiding her true self<br>
+                - Quick to adapt to a new world<br>
+                - Still loves nature<br><br>
+                No need to do anything about her. Fate is already bringing her through her course.`
+            }]
+        },{
+            name: "juniper",
+            type: "folder",
+            content: [{
+                name: "juniNotes",
+                type: "text",
+                content: `A ghost that seems to be haunting Shifty's house in limbo. She's made friends with him, however she's unable to do much in her state. She hasn't appeared in the collapse either.<br><br>`
+            }]
+        },{
+            name: "syntrex",
+            type: "folder",
+            content: [{
+                name: "syntrexNotes",
+                type: "text",
+                content: ``
+            }]
+        },{
+            name: "omega&omex",
+            type: "folder",
+            content: [{
+                name: "omegaNotes",
+                type: "text",
+                content: ``
+            },{
+                name: "omexNotes",
+                type: "text",
+                content: ``
+            }]
+        },{
+            name: "vixie",
+            type: "folder",
+            content: [{
+                name: "vixieNotes",
+                type: "text",
+                content: `Someone who originated from the same curse that came to Shifty.`
+            }]
+        },{
+            name: "theVariousGraces",
+            type: "folder",
+            content: []
         }]
     },{
         name: "notes",
         type: "folder",
         content: [{
-            name: "2026March4",
+            name: "2026March4th",
             type: "html",
             content: `
-                <p>Hello! My name is ██████. Or, well, that isn't my name, I just like censoring it to feel mysterious. This is a little project I've decided to make, ██████ OS! I named it that because I couldn't think of a better name... and I like feeling mysterious. Anyways, I just made this as my own personal way to catalog my notes, files, and more. Although nobody was ever meant to find this outside of myself, I wouldn't be surprised if someone manages to stumble upon this mess of spaghetti code. Either way, I do hope someone else finds the things I put here as interesting as I do!
+                <p>Hello! My name is █████. Or, well, that isn't my name, I just like censoring it to feel mysterious. This is a little project I've decided to make, ██████ OS! I named it that because I couldn't think of a better name... and I like feeling mysterious. Anyways, I just made this as my own personal way to catalog my notes, files, and more. Although nobody was ever meant to find this outside of myself, I wouldn't be surprised if someone manages to stumble upon this mess of spaghetti code. Either way, I do hope someone else finds the things I put here as interesting as I do!
                 <br><br>
-                - ██████</p>
+                - █████</p>
             `
+        },{
+            name: "2026March5th",
+            type: "html",
+            content: `
+                <p>Hello again! So remember when I said that I was making this for myself personally? Well, I kinda lied about that. I am planning to show this to a group of people at some point, a community of sorts. I have no idea if you are that group, or if you're someone else, but either way you reading this may or may not be intended. Anyways, I've also been planning to put a few things here and there, like maybe an RSS feed I could update whenever there's new details on the Warren's and Pinkbox situation? I dunno, either way I'll keep working on this. Still need to figure out what else to add, though... oh well, I'll just go ahead and see how people are reacting to Pokopia in the meantime. See if it's living up to the hype.
+                <br><br>
+                - █████</p>
+            `
+        },{
+            name: "2026March9th",
+            type: "html",
+            content: `
+                <p>Proof that I won't be consistently making notes. Anyways, I've decided to finally go ahead and start working on those notes. I would use full HTML to format them better, but also... text files are more convenient. I'm saving the HTML for these notes specifically, in case I wanna add images and stuff. Sucks, but I can work with it. Making a few general improvements to the OS as well, adding some features and bug fixes and all that.<br>
+                Also, Pokopia seems cool. Might try it sometime.
+                <br><br>
+                - █████</p>
+            `
+        },{
+            name: "2026March10th",
+            type: "html",
+            content: `
+                <p>I have been wondering on how I could have a way to notify people on an update to the website. I know I could just have a feed for devlogs or something, but I want it so that only the people who are intended to see this site will know when it gets updated, and have it convenient for them while still keeping it somewhat hidden for those who aren't intended to see any of this. Situations like these puzzle me, but I also enjoy them in a way. Taking challenging situations and attempting to solve them is very much something I love to do. Anyways, I'll probably make another note once I have an idea.
+                <br><br>
+                - █████<br>
+                P.S. Happy Mario day!</p>
+            `
+        },{
+            name: "2026March11th",
+            type: "html",
+            content: `<p>I've gotten an idea.<br>
+                I've been thinking of having some kind of question of the day thing. Except, I'm not really known for being consistent with that kind of stuff. Or really anything at all. However, I could use that kind of thing to have updates on the site! Whenever I make an update, I'll start the non-daily thing. People will chalk it up to me being infrequent, which is true, but those who know will immediately notice that there's something new on the site!<br>
+                ... I should really add a use for the notifications.
+                <br><br>
+                - █████<br></p>`
+        }]
+    },{
+        name: "",
+        type: "folder",
+        content: [{
+            name: "",
+            type: "folder",
+            content: [{
+                name: "",
+                type: "folder",
+                content: [{
+                    name: "",
+                    type: "html",
+                    content: ""
+                }]
+            }]
         }]
     }]
 }
+var options = {animations: true}
 
 var mainBody
+var animatedElements
 document.addEventListener('DOMContentLoaded', function() {
     var tickInterval = setInterval(tick, 100);
     var d = 0;
@@ -57,6 +193,8 @@ document.addEventListener('DOMContentLoaded', function() {
     loadWindows()
     mainBody = document.getElementById("body")
     closeFolder()
+    animatedElements = document.getElementsByClassName("animated")
+    console.log(animatedElements)
 });
 
 var notifsHidden = true;
@@ -100,8 +238,6 @@ function dragElement(element) {
     // Step 8: Set up event listeners for mouse movement (`elementDrag`) and mouse button release (`closeDragElement`).
     document.onmouseup = stopDragging;
     document.onmousemove = dragElement;
-    // Step 8.5: Add the selected class to the window
-    selectWindow(element)
   }
 
   // Step 9: Define the `elementDrag` function to calculate the new position of the element based on mouse movement.
@@ -125,23 +261,26 @@ function dragElement(element) {
   }
 }
 
+// Basic window functionality
 function closeWindow(closedWindow){
     document.getElementById(closedWindow).style.visibility = "hidden";
 }
 function openWindow(openedWindow){
     document.getElementById(openedWindow).style.visibility = "visible";
-    document.getElementById(openedWindow).style.top = "32px";
-    document.getElementById(openedWindow).style.left = "32px";
+    document.getElementById(openedWindow).style.top = window.event.clientY + "px";
+    document.getElementById(openedWindow).style.left = window.event.clientX-(window.innerWidth-window.innerHeight*(4/3))/2 + "px";
     selectWindow(document.getElementById(openedWindow))
+    startWindowAnim(document.getElementById(openedWindow))
 }
 function loadWindows(){
-    // Make the DIV element draggable once the element exists.
+    // Make the window elements draggable one by one.
     for(let i = 0; i < document.getElementsByClassName("window").length; i++){
         dragElement(document.getElementsByClassName("window")[i]);
         document.getElementsByClassName("window")[i].addEventListener("mousedown", () => selectWindow(document.getElementsByClassName("window")[i]));
     }
 }
 
+// Selecting/deselecting windows
 var maxZ = 0;
 function selectWindow(element){
     if(selectedWindow != undefined){
@@ -152,15 +291,46 @@ function selectWindow(element){
 
     maxZ++;
     selectedWindow.style.zIndex = maxZ;
-    console.log(selectedWindow)
 }
 function deselectWindow(element){
     element.classList.remove("selected");
 }
 
+// Window animation
+var lastUpdate = null
+var delta = 0
+var animatedWindow
+var animationOffset = 8
+var animationLength = 150.0
+function startWindowAnim(element){
+    if(options.animations){
+        animatedWindow = element
+        delta = 0
+        requestAnimationFrame(windowAnim)
+    }
+}
+function windowAnim(){
+    if(delta < animationLength){
+        if(lastUpdate == null){
+            lastUpdate = Date.now()
+        }
+        var now = Date.now()
+        delta += now - lastUpdate
+        lastUpdate = now
+
+        animatedWindow.style.transform = "translate(0, "+Math.round(animationOffset-(Math.sin(delta/animationLength*Math.PI/2))*animationOffset)+"px)"
+        requestAnimationFrame(windowAnim)
+    } else{
+        lastUpdate = null
+        animatedWindow.style.transform = "translate(0px, 0px)"
+    }
+}
+
+// Basic file navigation
 var currentFolder = []
 function openFile(fileLocation){
     fileSearch = files
+    console.log(fileLocation)
     for(let i = 0; i < fileLocation.length; i++){
         fileSearch = fileSearch.content[fileLocation[i]]
     }
@@ -176,12 +346,11 @@ function openFile(fileLocation){
     return fileSearch
 }
 function openRelativeFile(fileLocation){
-    file = openFile(currentFolder + fileLocation)
+    file = openFile(currentFolder.concat(fileLocation))
     if(file.type == "folder"){
-        currentFolder.push(fileLocation)
+        if(currentFolder[0] != 4 && currentFolder[1] != 0){currentFolder.push(fileLocation)}
         openFolder()
     }
-    console.log(file)
 }
 function closeFolder(){
     currentFolder.pop()
@@ -191,10 +360,11 @@ function closeFolder(){
     }
     return file
 }
-function getcurrentFolder(){
+function getCurrentFolder(){
     return openFile(currentFolder)
 }
 
+// Opening various files/folders
 function openFolder(openFolder = currentFolder){
     var folderWindow = document.getElementById("folderContent");
     folderWindow.innerHTML = ""
@@ -202,6 +372,10 @@ function openFolder(openFolder = currentFolder){
     for(let i = 0; i < folderContent.length; i++){
         folderWindow.innerHTML += "<p onclick='openRelativeFile(" + i + ")'><img class='fileIcons' src='" + folderContent[i].type + "-icon.png'>" + folderContent[i].name + "</p>"
     }
+    if(folderContent.length == 0){
+        folderWindow.innerHTML = "<p>This folder is empty...</p>"
+    }
+    startWindowAnim(document.getElementById("folderWindow"))
 }
 function openImage(currentFile){
     if(document.getElementById(currentFile.name + "Window") == null){
@@ -209,20 +383,24 @@ function openImage(currentFile){
             <div class="header" id="`+currentFile.name+`Windowheader"><img src="x.png" class="xButton" onclick="closeWindow('`+currentFile.name+`Window')"></div>
                 <img src="`+currentFile.content+`">
         </div>`
-        loadWindows()
         deselectWindow(document.getElementById("folderWindow"))
     }
+    loadWindows()
     openWindow(currentFile.name + "Window")
 }
 function openText(currentFile){
     if(document.getElementById(currentFile.name + "Window") == null){
         mainBody.innerHTML += `<div id="`+currentFile.name+`Window" class="window" style="visibility: hidden;">
-            <div class="header" id="`+currentFile.name+`Windowheader"><img src="x.png" class="xButton" onclick="closeWindow('`+currentFile.name+`Window')"></div><div class="windowContent">
-                <p contenteditable="true" spellcheck="false">`+currentFile.content+`</p>
-        </div></div>`
-        loadWindows()
+            <div class="header" id="`+currentFile.name+`Windowheader"><img src="x.png" class="xButton" onclick="closeWindow('`+currentFile.name+`Window')"></div>
+            <div class="windowContent"><p contenteditable="true" spellcheck="false">`+currentFile.content+`</p></div>
+        </div>`
         deselectWindow(document.getElementById("folderWindow"))
+    } else{
+        document.getElementById(currentFile.name + "Window").innerHTML = `
+            <div class="header" id="`+currentFile.name+`Windowheader"><img src="x.png" class="xButton" onclick="closeWindow('`+currentFile.name+`Window')"></div>
+            <div class="windowContent"><p contenteditable="true" spellcheck="false">`+currentFile.content+`</p></div>`
     }
+    loadWindows()
     openWindow(currentFile.name + "Window")
 }
 function openContent(currentFile){
@@ -231,8 +409,45 @@ function openContent(currentFile){
             <div class="header" id="`+currentFile.name+`Windowheader"><img src="x.png" class="xButton" onclick="closeWindow('`+currentFile.name+`Window')"></div>
                 <div class="windowContent">`+currentFile.content+`</div>
             </div>`
-        loadWindows()
         deselectWindow(document.getElementById("folderWindow"))
     }
+    loadWindows()
     openWindow(currentFile.name + "Window")
+}
+
+function shakeBody(){
+    mainBody.style.transform = "translate("+(Math.random()*12-6)+"px, "+(Math.random()*12-6)+"px)"
+    requestAnimationFrame(shakeBody)
+}
+
+// Option related functions
+function toggleOption(option){
+    if(options[option]){
+        options[option] = false
+        document.getElementById(option+"Checkbox").src = "box-unchecked.png"
+        console.log(document.getElementById(option+"Checkbox"))
+
+        if(option == "animations"){
+            for(let i = 0; i < animatedElements.length; i++){
+                animatedElements[i].classList.replace("animated", "unanimated")
+                console.log(animatedElements[i] + " " + i)
+            }
+            console.log(animatedElements)
+        }
+    } else{
+        options[option] = true
+        document.getElementById(option+"Checkbox").src = "box-checked.png"
+        console.log(document.getElementById(option+"Checkbox"))
+
+        if(option == "animations"){
+            for(let i = 0; i < animatedElements.length; i++){
+                animatedElements[i].classList.replace("unanimated", "animated")
+                console.log(animatedElements[i] + " " + i)
+            }
+        }
+    }
+}
+var optionsHidden = true;
+function showHideOptions(){
+
 }
